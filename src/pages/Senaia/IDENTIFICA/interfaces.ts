94e6f48cb2abbdfda02b8a3dca3bf8d7e1df3624
roheka <roheka.app@gmail.com>
2023-08-07 15:12:44 -0400

    feat: interfaces update

--- src/pages/Senaia/IDENTIFICA/interfaces.ts
+++ src/pages/Senaia/IDENTIFICA/interfaces.ts
@@ -1,3 +1,193 @@
+export interface UsuarioDTO {
+  user_vpa: string;
+  email: string;
+  password: string;
+  ids?: string;
+  user_id?: string;
+  nome?: string;
+  senha?: string;
+  telefone?: string;
+  cpf?: string;
+  firstname?: string;
+  lastname?: string;
+  user_company?: string;
+  user_name?: string;
+  user_register?: string;
+  user_password?: string;
+  user_mail?: string;
+  user_phone?: string;
+  user_cpf?: string;
+  user_pos_id?: string;
+  user_levelaccess?: string;
+  user_acreditacion?: string;
+  user_apikeymobile?: string;
+  user_servername?: string;
+  user_devicestatus?: string;
+  user_keyprivate?: string;
+  user_apikeyhardware?: string;
+  user_apikeysoftware?: string;
+  user_messages?: string;
+  user_passwordremote?: string;
+  user_groupusers?: string;
+  user_issync?: string;
+  user_uniqueid?: string;
+  user_vpa_id?: string;
+  user_key?: string;
+  user_name_register?: string;
+  user_status?: string;
+  user_locked?: string;
+  user_key_hardware?: string;
+  user_level_access?: string;
+  user_token?: string;
+  user_public_key?: string;
+  user_private_key?: string;
+  user_wallet_id?: string;
+  user_system_type?: string;
+  user_mail_verify?: string;
+  user_phone_verify?: string;
+  user_professional_type?: string;
+  user_gender?: string;
+  user_born_date?: string;
+  user_nation?: string;
+  user_photo_ci?: string;
+  user_photo?: string;
+  user_bank_name?: string;
+  user_bank_user_name?: string;
+  user_bank_account?: string;
+  user_bank_agency?: string;
+  user_bank_user_cpf?: string;
+  user_date_register?: string;
+  user_otp_verify?: string;
+  user_latitude?: string;
+  user_longitude?: string;
+  user_coordinates?: string;
+  user_number_services?: string;
+  Status?: string;
+  servicos?: string;
+  user_issinc?: string;
+  counter?: number;
+}
+export interface UserCategoryDTO {
+  user_id: number | undefined;
+  key: number | undefined;
+  title: string;
+  name: string;
+  address: string;
+  price: string;
+  description: string;
+  photo: string;
+  star: string;
+  reviews: string;
+  category: string;
+  img: string;
+  other: string;
+  dollar: string;
+  cleaner: string;
+  usersCategory_sync: boolean;
+}
+
+export interface CoibfePropriedadDTO {
+  id?: number;
+  created_at?: number;
+  updated_at?: number;
+  dbversion?: string;
+  propriedad_id?: string;
+  propriedadname: string;
+  propriedadpropietario?: string;
+  propriedadstatus?: string;
+  propriedadsigor: string;
+  propriedadsitrap?: string;
+  propriedaddepartamento?: string;
+  propriedaddistrito?: string;
+  propriedadproductors?: string;
+}
+export interface CoibfeProductorDTO {
+  id?: number;
+  created_at?: number;
+  updated_at?: number;
+  dbversion?: string;
+  productorname: string;
+  productor_id: string;
+  productortoken?: string;
+  productorsitrap?: string;
+  productoracreditacion?: string;
+  productor_propriedad_id?: string;
+  productorpassword?: string;
+  productormail?: string;
+  productorphone?: string;
+  productorissync?: string;
+
+  productordocnroprop?: string;
+  productordocdigprop?: string;
+  productordocorigabrev?: string;
+  productordoctipoabrev?: string;
+  productorstatus?: string;
+  productormessages?: string;
+  productorkeyprivate?: string;
+  productorapikeysoftware?: string;
+}
+
+export interface CoibfeFrigorificoDTO {
+  id?: number;
+  created_at?: number;
+  updated_at?: number;
+  dbversion?: string;
+  frigorificoname: string;
+  frigorifico_id: string;
+  frigorificodepartamento?: string;
+  frigorificokeyprivate?: string;
+  frigorificostatus?: string;
+}
+
+export interface CoibfeCoibfeDTO {
+  dbversion?: string;
+  coibfeid: string;
+  coibfekey?: string;
+  coibfetoken?: string;
+  coibfecodigov: string;
+  coibfedestino: string;
+  coibfefinalidad: string;
+  coibfetransporte: string;
+  coibfeaninovillos: string;
+  coibfeAniToros: string;
+  coibfeanivacas: string;
+  coibfeanivaquillas: string;
+  coibfeaniotros: string;
+  coibfeanitotal: string;
+  coibfeanihilton: string;
+  coibfetecnico_vpa_id: string;
+  coibfetecniconame: string;
+  coibfefrigorificoname: string;
+  coibfefrigorifico_id: string;
+  coibfeproductorname: string;
+  coibfeproductor_id: string;
+  coibfeproductorsitrap?: string;
+  coibfepropriedadname: string;
+  coibfepropriedad_id?: string;
+  coibfepropriedadsigor: string;
+  coibfepropriedadsitrap?: string;
+  coibfepropriedaddepartamento?: string;
+  coibfepropriedaddistrito?: string;
+  coibfepropriedad_productor_id?: string;
+  coibfeprecinto1: string;
+  coibfeprecinto2?: string;
+  coibfeprecinto3?: string;
+  coibfepos_id?: string;
+  coibfeposlatitud: string;
+  coibfeposlongitud: string;
+  coibfeposdate?: string;
+  coibfeposapikeymobile?: string;
+  coibfeobs?: string;
+  coibfedocnroprop?: string;
+  coibfedocdigprop?: string;
+  coibfedocorigabrev?: string;
+  coibfedoctipoabrev?: string;
+  coibfeerrocode?: string;
+  coibfeerromessage?: string;
+  coibfeanimales?: string;
+  coibfe_issinc?: string;
+}
+
 export interface VacunaDTO {
   dbversion?: string;
   vacunaId?: string;
